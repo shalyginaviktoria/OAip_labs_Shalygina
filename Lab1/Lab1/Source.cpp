5c636ffa51f1709605dd7a9a4c24b9cab78384f4
shalyginaviktoria <shalygina.vika@gmail.com>
2017-09-27 17:25:51 +0300

    17:25commit

--- Lab1/Lab1/Source.cpp
+++ Lab1/Lab1/Source.cpp
@@ -2,30 +2,23 @@
 #include <stdio.h>
 #include <iostream>
 using namespace std;
-
+#define KMPH 1.852;
+#define MPS 3.6;
 
 int main()
 {
-	
 	float a;
-	printf("Enter the speed in the node ", &a);
-	a = scanf("%f", &a);
-	float b = a*1.852;
-	
-	
+	printf("Enter the speed in the node ");
+	scanf("%f", &a);
+	float b = a * KMPH;
 	if (a == 0) {
-		printf("Error: 'a' must be integer");
+		printf("Error: speed must be float");
 	}
 	else {
-
-		
-
-		printf("Speed in h / h %9.2f  \n", b);
-		float c = (b * 1000) / 3600;
+		printf("Speed in km / h %9.2f  \n", b);
+		float c = b * MPS;
 		printf("Speed in m / s %9.2f  \n", c);
-	}
-
-
+	};
 	system("Pause");
 
 }
