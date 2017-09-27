5e3d9e589a819dbd6ca59e8afdff0e365c1a5947
shalyginaviktoria <shalygina.vika@gmail.com>
2017-09-27 12:41:25 +0300

    message

---
+++ Lab1/Lab1/Source.cpp
@@ -0,0 +1,32 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <conio.h>
+using namespace std;
+
+
+int main()
+{
+	setlocale(0, "Russian");
+	float b, c, a, D;
+	printf("Ââåäèòå ñêîðîñòü â óçëàõ \n");
+	scanf("%f", &a);
+	b = a*1.852;
+	int f;
+	f = scanf("%d", &a);
+	if (f == 0) {
+		printf("Error: 'a' must be integer");
+	}
+	else {
+
+		printf("D = %d", a);
+
+		printf("Ñêîðîñòü â êì/÷ %9.2f  \n", b);
+		c = (b * 1000) / 3600;
+		printf("Ñêîðîñòü â ì/c %9.2f  \n", c);
+	}
+
+
+	getch();
+
+}
+
