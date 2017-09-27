083e8ff0cf9c894d8f719affb42631036a798442
shalyginaviktoria <shalygina.vika@gmail.com>
2017-09-27 13:05:17 +0300

    message

--- Lab1/Lab1/Source.cpp
+++ Lab1/Lab1/Source.cpp
@@ -1,32 +1,32 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <conio.h>
+#include <iostream>
 using namespace std;
 
 
 int main()
 {
-	setlocale(0, "Russian");
-	float b, c, a, D;
-	printf("Ââåäèòå ñêîðîñòü â óçëàõ \n");
-	scanf("%f", &a);
-	b = a*1.852;
-	int f;
-	f = scanf("%d", &a);
-	if (f == 0) {
+	
+	float a;
+	printf("Enter the speed in the node ", &a);
+	a = scanf("%f", &a);
+	float b = a*1.852;
+	
+	
+	if (a == 0) {
 		printf("Error: 'a' must be integer");
 	}
 	else {
 
-		printf("D = %d", a);
+		
 
-		printf("Ñêîðîñòü â êì/÷ %9.2f  \n", b);
-		c = (b * 1000) / 3600;
-		printf("Ñêîðîñòü â ì/c %9.2f  \n", c);
+		printf("Speed in h / h %9.2f  \n", b);
+		float c = (b * 1000) / 3600;
+		printf("Speed in m / s %9.2f  \n", c);
 	}
 
 
-	getch();
+	system("Pause");
 
 }
 
