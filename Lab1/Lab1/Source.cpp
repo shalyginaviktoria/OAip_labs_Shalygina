cc66b7d8dff494037ac4259ea5b4353a15fae464
shalyginaviktoria <shalygina.vika@gmail.com>
2017-10-13 12:36:55 +0300

    итоговая версия кода

--- Lab1/Lab1/Source.cpp
+++ Lab1/Lab1/Source.cpp
@@ -1,12 +1,10 @@
-#define _CRT_SECURE_NO_WARNINGS
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <conio.h>
 #include <windows.h>
 
-
-using namespace std;
-#define KilometersPerHour 1.852
-#define МetersPerSecond 3.6
+#define KILOMETERS_PER_HOUR 1.852
+#define МETERS_PER_SECOND 3.6
 
 int main()
 {
@@ -21,10 +19,10 @@ int main()
 		printf("Ошибка: скорость должна быть вещественым числом");
 	}
 	else {
-		float speedKM = speed * KilometersPerHour; 
+		float speedKM = speed * KILOMETERS_PER_HOUR;
 		printf("Скорость в км / ч %9.2f  \n", speedKM);
 
-		float speedMS = speedKM * МetersPerSecond;
+		float speedMS = speedKM * МETERS_PER_SECOND;
 		printf("Скорость в м / с %9.2f  \n", speedMS);
 	     }
 	_getch();
